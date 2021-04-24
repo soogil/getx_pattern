6f62c60057d05f4d9e64b8b2dd84d2cfd08b633a
kimsoogil <zmfflrql5@gmail.com>
2021-04-25 01:40:44 +0900

    [soogil] GetXController // GetXService 차이

--- lib/controller/ex_controller.dart
+++ lib/controller/ex_controller.dart
@@ -21,3 +21,23 @@ class ExController extends GetxController {
   String get title => _exModel.value.title ?? '';
   String get body => _exModel.value.body ?? '';
 }
+
+class ExGetXService extends GetxService { // controller 와 차이는 초기화가 안돼서 전역? 으로 사용 가능 초기화 하고싶을땐 Get.reset() 사용
+  static ExController get to => Get.find(); // ExController.to로 UI 에서 접근 가능
+
+  final ExRepository _exRepository = ExRepository();
+
+  var _exModel = ExModel().obs;
+
+  @override
+  void onInit() async {
+    // print('onInit ${_item.value.title}');
+    _exModel.value = await _exRepository.readData;
+
+    super.onInit();
+  }
+
+  String get id => _exModel.value.id ?? '';
+  String get title => _exModel.value.title ?? '';
+  String get body => _exModel.value.body ?? '';
+}
